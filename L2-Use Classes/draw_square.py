d50dc6a30c99e86c523476e23a4a7946b1351efd
SeaBass <sir0064@gmail.com>
2016-06-03 23:54:47 -0400

    Updated draw_square and added check_profanity

--- L2-Use Classes/draw_square.py
+++ L2-Use Classes/draw_square.py
@@ -1,19 +1,28 @@
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+        
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("red")
     
     brad = turtle.Turtle()
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+    brad.shape("turtle")
+    brad.color("green")
+    brad.speed(4)
+    
+    for i in range(1,37):
+        draw_square(brad)
+        brad.right(10)
+    
+    #angie = turtle.Turtle()
+    #angie.shape("arrow")
+    #angie.color("blue")
+    #angie.circle(100)
     
     window.exitonclick()
     
-draw_square()
+draw_art()
